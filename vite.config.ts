3b0d2d1378a3fa8204777e19e27002850f946024
mato1321 <88338776+mato1321@users.noreply.github.com>
2026-07-30 01:08:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   base: '/MY-CV/',
   tanstackStart: {
-    server: { entry: "server" },
-  },
-  // 加上 Nitro 的純靜態預渲染設定，專門給 GitHub Pages 等靜態託管平臺使用
-  nitro: {
-    static: true,
-    prerender: {
-      crawlLinks: true,
-      routes: ['/'],
+    // 讓 TanStack 知道 baseUrl
+    output: {
+      dir: '.output',
     },
+    server: { entry: "server" },
   },
 });
