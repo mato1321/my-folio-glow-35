ec215b7299c17971d541424c7a0767f364a655f6
mato1321 <88338776+mato1321@users.noreply.github.com>
2026-07-30 00:38:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  base: '/MY-CV/',
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    base: '/MY-CV/',
     server: { entry: "server" },
   },
 });
